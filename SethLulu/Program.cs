a41249af23e11ae1d5c26ef76bef9f82f42e7def
bl4ck1 <marcolamers@gmx.de>
2015-04-28 23:43:54 +0200

    Update Program.cs

--- SethLulu/Program.cs
+++ SethLulu/Program.cs
@@ -120,7 +120,7 @@ namespace SethLulu
             {
                 Utility.HpBarDamageIndicator.Enabled = eventArgs.GetNewValue<bool>();
             };
-            Game.OnGameUpdate += Game_OnGameUpdate;
+            Game.OnUpdate += Game_OnGameUpdate;
             Drawing.OnDraw += Drawing_OnDraw;
             Interrupter2.OnInterruptableTarget += Interrupter2_OnInterruptableTarget;
             AntiGapcloser.OnEnemyGapcloser += AntiGapcloserOnOnEnemyGapcloser;
